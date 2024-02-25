5bf2b33f207d9bf4697d6b0552ce21a4000462f9
MOHAMED MOUROUH <60442896+m-mourouh@users.noreply.github.com>
2024-02-25 17:04:31 +0100

    Update About.styled.ts

--- styles/styled-components/About.styled.ts
+++ styles/styled-components/About.styled.ts
@@ -130,10 +130,7 @@ export const StyledAbout = styled.section`
                 height: 100%;
                 border: 4px solid var(--white);
                 border-radius: 50%;
-                filter: grayscale(0.7);
-                &:hover {
-                    filter: grayscale(0);
-                }
+                background: repeating-linear-gradient(45deg, #7eee14, #1a1a1a 100px);
             }
 
             .icon {
